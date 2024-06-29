b61cd424819725f1e0f99d4ee02deb49cfac1c11
jongcoding <ialleejy@gmail.com>
2024-06-29 16:31:40 +0900

    UPDATE BOJ C 11654

--- 이종윤/C/11654.c
+++ 이종윤/C/11654.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    char cha;
-    scanf("%c", &cha); 
-    printf("%d\n", cha); 
+    int n;
+    scanf("%d", &n);  
+    char number[n + 1];  
+    
+    scanf("%s", number);  
+
+    int result = 0;  
+    
+    
+    for (int i = 0; i < n; i++) {
+        result += number[i] - '0';  
+    }
+
+    printf("%d\n", result);  
     return 0;
 }
