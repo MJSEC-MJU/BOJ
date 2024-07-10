ef7b7f587f9391c32793b76be9223aeb63332f54
jongcoding <ialleejy@gmail.com>
2024-07-11 00:00:48 +0900

    update boj c 5597

--- 이종윤/C/2week/5597.c
+++ 이종윤/C/2week/5597.c
@@ -1,1 +1,22 @@
-#include <stdi
+#include <stdio.h>
+int main(void) {
+    int num[30],student,count=0;
+
+    for(int i=0;i<30;i++) {
+        num[i]=0;
+    }
+    for(int i=0;i<28;i++) {
+        scanf("%d",&student);
+        num[student-1]=1;
+    }
+    for(int i=0;i<30;i++) {
+        if (count==2) {
+            return 0;
+        }
+        if (num[i]==0) {
+            printf("%d\n",i+1);
+            count++;
+        }
+    }
+    
+}
